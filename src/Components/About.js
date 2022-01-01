bec5fe7de8912a172b1282a9dd996bd3c5489900
Gregory Banks <gbanks1438@gmail.com>
2022-01-01 17:43:44 -0500

    Updated the About page content

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,8 +13,20 @@ function About() {
           <h4 class="green-neon-text">Located on Big Bowman Pond in upstate NY.</h4>
           <br/>
           <br/>
-          <p class="blue-shadow"> Our den is avilable to family and friends for weekends, getaways, and holiday excursions.</p>
-          <p class="blue-shadow">Please use this site to book dates in advance to avoid conflicting plans.</p>
+          <p class="blue-shadow">Our 'camp' is avilable to family and friends for weekends, getaways, and holiday excursions.</p>
+          <p class="blue-shadow">The house features expansive water views, 5 fireplaces, and a 3 car garage.</p>
+          <p class="blue-shadow">There are modern amenities like a dishwasher, laundry, automatic backup generator, highspeed Wi-Fi, and multi-room audio for music.</p>
+          <br/>
+          <p class="blue-shadow">On the main floor you will find the kitchen, an office, a half bathroom, the formal dining room, the master bedroom with ensuite, access to the back deck, and the livingroom with the grand fireplace.</p>
+          <p class="blue-shadow">The second floor has three bedrooms and a full bathroom.</p>
+          <p class="blue-shadow">In the basement we have a cozy den with an entertainment center and a large screen HD TV, as well as a full bathroom.</p>
+          <p class="blue-shadow">There is also a game room with a full sized pool table and a full kitchen.</p>
+          <p class="blue-shadow">Off the back of the house is a ground level screened in porch with an outdoor shower.</p>
+          <br/>
+          <p class="blue-shadow">We have everything available for kayaking, hiking, swimming, and fishing.</p>
+          <p class="blue-shadow">And we also have stacks of dry, aged, and split firewood.</p>
+          <br/>
+          <p class="blue-shadow">Please visit the Reservations tab to book dates.</p>
           </div>
           <br/>
         <br/>
